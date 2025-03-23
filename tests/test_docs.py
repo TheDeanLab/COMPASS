8557cb4b85e7976abfb8620de23215cc3f704129
Kevin M. Dean <kevin.dean@utsouthwestern.edu>
2025-03-23 13:35:27 -0500

    Update test_docs.py

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,25 +1,38 @@
-import os
+import re
 import unittest
 import subprocess
 import os
 
 
 class TestDocs(unittest.TestCase):
-    def test_sphinx_linkcheck(self):
+    def setUp(self):
         os.chdir(os.path.join(os.getcwd(), 'docs'))
         print("Current Directory:", os.getcwd())
-        print("Files in Directory:", os.listdir("."))
+
+    def test_sphinx_build(self):
         cmd = [
             "make",
             "linkcheck",
         ]
 
+        # Run the build, and print any results.
         result = subprocess.run(cmd, capture_output=True, text=True)
-
-        # Log the output to see which links are failing.
         print(result.stdout)
         print(result.stderr)
+
+        # Check that no links are broken.
         assert result.returncode == 0, "Broken links found in Sphinx documentation!"
+
+        # Check that all images are found.
+        missing_image_pattern = re.compile(
+            r"WARNING: image file not readable",
+            re.IGNORECASE)
+        match = missing_image_pattern.search(result.stderr)
+        self.assertIsNone(
+            match,
+            "There are missing images in the documentation! "
+            "Check Sphinx warnings above."
+        )
 
 
 if __name__ == '__main__':
